88c80be2f779cf7bb02503a04f848ae7e90a08fd
Abdullah <abdullah.4mohamed@gmail.com>
2024-09-28 02:08:43 +0100

    fix the nod g policy

--- tf/modules/iam/iam-module.tf
+++ tf/modules/iam/iam-module.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "node_role" {
     Statement = [{
       Action = "sts:AssumeRole"
       Principal = {
-        Service = "ec2.amazonaws.com"
+        Service = "eks-nodegroup.amazonaws.com"
       }
       Effect = "Allow"
     }]
