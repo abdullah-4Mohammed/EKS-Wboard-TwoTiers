3c1b3caa3bc5eb90aa7a8c16f510d5c5e00a2ed9
Abdullah <abdullah.4mohamed@gmail.com>
2024-10-01 18:00:27 +0100

    all good

--- tf/modules/network/network-module.tf
+++ tf/modules/network/network-module.tf
@@ -64,6 +64,30 @@ resource "aws_route_table_association" "public_route_assoc" {
 }
 
 
+resource "aws_eip" "nat" {
+  
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_route_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
 
 
 output "vpc_id" {
