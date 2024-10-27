a1c65cfd1cabc6dc1e5e4cc5681a92642d7f5536
Abdullah <abdullah.4mohamed@gmail.com>
2024-10-27 19:07:01 +0000

    changing the app completly

--- src/frontend/plugins/i18n.js
+++
@@ -1,9 +0,0 @@
-import WhitebirdLogger from '~/utils/WhitebirdLogger';
-
-const logger = new WhitebirdLogger('PLUGIN: i18n');
-
-export default ({ app }) => {
-  app.i18n.onLanguageSwitched = (oldLocale, newLocale) => {
-    logger.log(`OldLocale: ${oldLocale} -> NewLocale: ${newLocale}`);
-  };
-};
